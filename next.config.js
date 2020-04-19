4f125c587c50be50ac2d510bdcd0c16a49219a52
Louis <louisandrew3@gmail.com>
2020-04-19 15:43:54 +0200

    hid the Google API Key LOLOLOL

--- next.config.js
+++ next.config.js
@@ -12,13 +12,4 @@ module.exports = {
 
     return config
   },
-  env: {
-    apiKey: "AIzaSyDZ-Dnz23u4YQqyIAr7g_sKOyq24aMmjio",
-    authDomain: "nextfire-de283.firebaseapp.com",
-    databaseURL: "https://nextfire-de283.firebaseio.com",
-    projectId: "nextfire-de283",
-    storageBucket: "nextfire-de283.appspot.com",
-    messagingSenderId: "564559762912",
-    appId: "1:564559762912:web:76eb41342b91ff8d878785"
-  }
 }
